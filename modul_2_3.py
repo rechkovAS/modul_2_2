c20471272490d839fe75ea4eb57b2b6285331371
rechkovAS <alekseyrechkov@gmail.com>
2024-10-23 23:58:13 +0300

    Add files via upload

---
+++ modul_2_3.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+size = len(my_list)
+i = 0
+while i < size:
+    if my_list[i] > 0:
+        print(my_list[i])
+        i = i + 1
+    elif my_list[i] == 0:
+        i = i + 1
+    else:
+        break
+
